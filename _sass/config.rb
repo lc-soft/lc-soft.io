fae475f1c203f72bcb86502953695a01c48ef005
Liu Chao <lc-soft@live.cn>
2016-01-31 20:56:11 +0800

    修改 sass 配置

--- _sass/config.rb
+++ _sass/config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = ""
-sass_dir = "sass"
+css_dir = "../"
+sass_dir = "."
 images_dir = "../images"
 javascripts_dir = ""
 
