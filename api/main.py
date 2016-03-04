f06ead97018922e976bb3193c6906ea2fbb89dc0
Liu Chao <lc-soft@live.cn>
2016-03-04 19:14:18 +0800

    补充命令行输出结果

--- api/main.py
+++ api/main.py
@@ -5,12 +5,15 @@ app = Flask(__name__)
 
 @app.route('/update', methods=['POST'])
 def update():
+    logs = []
     data = request.get_json()
+    cmds = ['git pull master', 'jekyll build']
     if data['repository']['full_name'] != 'lc-soft/lc-soft.io':
         abort(400)
-    os.system('git pull master')
-    os.system('jekyll build')
-    return jsonify({'message': 'ok'})
+    for cmd in cmds:
+        log = os.popen(cmd)
+        logs.append({'command': cmd, 'output': log.readlines()})
+    return jsonify({'logs': logs})
 
 if __name__ == '__main__':
     app.run()
