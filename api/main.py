2457966c5b2dfdf619862ea22912b33faf2515fe
Liu Chao <lc-soft@live.cn>
2016-10-14 23:13:06 +0800

    更正语法错误

--- api/main.py
+++ api/main.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 @app.route('/update', methods=['POST'])
 def update():
-    log = ''
     data = request.get_json()
     if data['repository']['full_name'] != 'lc-soft/lc-soft.io':
         abort(400)
-    for cmd in cmds:
-        log = os.popen('su lc-soft -c "sh ./api/update.sh"')
+    log = os.popen('su lc-soft -c "sh ./api/update.sh"')
     return jsonify({'log': log})
 
 if __name__ == '__main__':
