6278811f43f58b70da7370e0ec7820086d6a0619
Liu Chao <lc-soft@live.cn>
2016-11-05 09:48:18 +0800

    纠正 os.popen() 返回的数据的读取方式

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ def update():
     if data['repository']['full_name'] != 'lc-soft/lc-soft.io':
         abort(400)
     log = os.popen('su - liuchao -m -c "sh ./api/build.sh"')
-    return jsonify({'log': log})
+    return jsonify({'log': log.read()})
 
 if __name__ == '__main__':
     app.run()
