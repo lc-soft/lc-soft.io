615dfc2d3e148c8cb7c8e2f755dd14a6257b2119
Liu Chao <lc-soft@live.cn>
2016-11-05 09:51:16 +0800

    将 build.sh 执行结果拆分成多行，方便阅读

--- api/main.py
+++ api/main.py
@@ -9,7 +9,7 @@ def update():
     if data['repository']['full_name'] != 'lc-soft/lc-soft.io':
         abort(400)
     log = os.popen('su - liuchao -m -c "sh ./api/build.sh"')
-    return jsonify({'log': log.read()})
+    return jsonify({'logs': log.readlines()})
 
 if __name__ == '__main__':
     app.run()
