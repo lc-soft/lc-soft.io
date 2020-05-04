6ea121ffb07283fb909a8290cc7e51885a21396d
Liu <lc-soft@live.cn>
2020-05-04 18:32:32 +0800

    异步执行构建任务，让接口尽早返回结果

--- api/main.py
+++ api/main.py
@@ -1,15 +1,20 @@
 import os
 from flask import Flask, jsonify, request, abort
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
+executor = ThreadPoolExecutor(1)
 
 @app.route('/build', methods=['POST'])
-def update():
+def build():
     data = request.get_json()
     if data['repository']['full_name'] != 'lc-soft/lc-soft.io':
         abort(400)
-    log = os.popen('sh ./api/build.sh')
-    return jsonify({'logs': log.readlines()})
+    executor.submit(run_build)
+    return jsonify({ 'message': 'The build task has started' })
+
+def run_build():
+    os.popen('sh ./api/build.sh')
 
 if __name__ == '__main__':
     app.run()
